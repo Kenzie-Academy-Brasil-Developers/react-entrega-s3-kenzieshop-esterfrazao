4a55970d160ba227ef9da46494ff1a1d60d1ba5a
esterfrazao <tafnisbonbon@gmail.com>
2022-04-29 14:32:02 -0300

    Feat: Create React App

---
+++ src/database/index.js
@@ -0,0 +1,14 @@
+const products = [
+  { img: "../assets/abelha.jpeg" },
+  { img: "../assets/gato.jpeg" },
+  { img: "../assets/hamster.jpeg" },
+  { img: "../assets/lobo.jpeg" },
+  { img: "../assets/mochila-coelho.jpeg" },
+  { img: "../assets/mochila-infantil.jpeg" },
+  { img: "../assets/panda.jpeg" },
+  { img: "../assets/porco.jpeg" },
+  { img: "../assets/princesa-caroco.jpeg" },
+  { img: "../assets/urso.jpeg" },
+];
+
+export default products;
