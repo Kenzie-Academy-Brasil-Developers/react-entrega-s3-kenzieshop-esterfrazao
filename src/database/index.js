02ec8a986bec102be2d2294dd6d2120e4ea72d24
esterfrazao <tafnisbonbon@gmail.com>
2022-04-29 16:00:42 -0300

    Feat: Database

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,73 @@
 const products = [
-  { img: "../assets/abelha.jpeg" },
-  { img: "../assets/gato.jpeg" },
-  { img: "../assets/hamster.jpeg" },
-  { img: "../assets/lobo.jpeg" },
-  { img: "../assets/mochila-coelho.jpeg" },
-  { img: "../assets/mochila-infantil.jpeg" },
-  { img: "../assets/panda.jpeg" },
-  { img: "../assets/porco.jpeg" },
-  { img: "../assets/princesa-caroco.jpeg" },
-  { img: "../assets/urso.jpeg" },
+  {
+    name: "Abelha",
+    description:
+      "Uma abelha de pelúcia, perfumada das flores que busca em seu dia de dia de trabalho de pelúcia",
+    price: 19.99,
+    img: "https://i.ibb.co/8d8XNnQ/abelha.jpg",
+  },
+  {
+    name: "Gato",
+    description:
+      "Um felino para aqueles que não possuem condições de manter um de verdade",
+    price: 45.5,
+    img: "https://i.ibb.co/wd28DRr/gato.jpg",
+  },
+  {
+    name: "Hamster",
+    description:
+      "Esse hamster é muito maior que parece e te fará companhia fofinhas",
+    price: 18.0,
+    img: "https://i.ibb.co/jJVwLxz/hamster.jpg",
+  },
+  {
+    name: "Lobo",
+    description: "Não tenha medo, ele não morde, apenas quer carinho",
+    price: 39.9,
+    img: "https://i.ibb.co/XSsPfY4/lobo.jpg",
+  },
+  {
+    name: "Mochila de Coelho",
+    description:
+      "Essa mochila macia e fofinha vai combinar com seus melhores looks e ainda irá manter suas costas aquecidas",
+    price: 56.0,
+    img: "https://i.ibb.co/mc4mmHq/mochila-coelho.jpg",
+  },
+  {
+    name: "Mochila Pelúcia Média",
+    description:
+      "Essa mochila é mais rígida para proteger melhor seu conteúdo, mas sem perder a fofura de uma pelúcia",
+    price: 60.0,
+    img: "https://i.ibb.co/wdFGP3W/mochila-infantil.jpg",
+  },
+  {
+    name: "Panda",
+    description:
+      "Leve essa fofura oriental para ser seu companheiro de brincadeiras e sonecas",
+    price: 114.95,
+    img: "https://i.ibb.co/sjJgtyC/panda.jpg",
+  },
+  {
+    name: "Porco",
+    description:
+      "Este não guarda moedas, mas pode ser um ótimo investimento para o seu conforto",
+    price: 15.4,
+    img: "https://i.ibb.co/SB9qXyY/porco.jpg",
+  },
+  {
+    name: "Princesa Caroço",
+    description:
+      "Da nossa coleção de personagens, a princesa caroço para te fazer companhia",
+    price: 50.0,
+    img: "https://i.ibb.co/sbKXDGH/princesa-caroco.jpg",
+  },
+  {
+    name: "Urso",
+    description:
+      "O clássico das pelúcias, senhor urso, novinho e cheiroso só pra vocÊ",
+    price: 20.0,
+    img: "https://i.ibb.co/Zdr7CCj/urso.jpg",
+  },
 ];
 
 export default products;
